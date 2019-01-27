d0d1cd8f20f07934b39a409dd0aa4060f46e4ef4
nagatax <4foview@gmail.com>
2019-01-27 15:27:45 +0900

    定数クラスの追加
    定数クラスを追加する。

---
+++ src/Value/Date.js
@@ -0,0 +1,22 @@
+/**
+ * @namespace
+ */
+var Value = Value || {};
+
+/**
+ * @constructor
+ * @classdesc 日付/時刻関連の
+ */
+Value.Date.prototype = {
+  WEEK: {
+    SUNDAY: Symbol(),
+    MONDAY: Symbol(),
+    TUESDAY: Symbol(),
+    WEDNESDAY: Symbol(),
+    THURSDAY: Symbol(),
+    FRIDAY: Symbol(),
+    SATURDAY: Symbol(),
+  },
+};
+
+module.exports = Value;
