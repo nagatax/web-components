ef2639808ad36a6f70907e5c820234704efc9ab4
nagatax <4foview@gmail.com>
2019-02-16 20:40:55 +0900

    デフォルト引数をチェックする関数の追加
    デフォルト引数をチェックする関数を追加する。

---
+++ src/Validator/Function.js
@@ -0,0 +1,25 @@
+/**
+ * @namespace
+ */
+var Validator = Validator || {};
+
+/**
+ * @constructor
+ */
+Validator.Function = function () {
+};
+
+Validator.Function = {
+  /**
+   * 関数の引数チェック
+   * 引数の必須項目のデフォルト値として設定する
+   *
+   * @example
+   * function( requiredValue = required(), notRequiredValue = undefined) {}
+   */
+  required() {
+    throw new Error('引数が不足しています');
+  },
+};
+
+module.exports = Validator;
