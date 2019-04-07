e18fd63b6fa576b8bae92083bb4473e21fa25989
nagatax <4foview@gmail.com>
2019-04-07 13:23:30 +0900

    Ajaxクラスの修正
    POSTメソッドを送信できるように修正する。

--- src/Util/Ajax.js
+++ src/Util/Ajax.js
@@ -1,38 +1,81 @@
+import { ENCTYPE, REQUEST_METHOD, RESPONSE_STATUS_CODE } from '../Value/Http';
+
 export default class Ajax {
-  constructor(url, method = 'GET') {
-    this.xhr = new XMLHttpRequest();
-    this.url = url;
-    this.method = method;
+  /**
+   * リソースを取得する。
+   * @param {String} url
+   * @param {Array} headers
+   */
+  get(url, headers = undefined) {
+    this.send(REQUEST_METHOD.GET, url, headers);
+  }
+
+  /**
+   * リソースを作成する。
+   * @param {String} url
+   * @param {Object|String} body
+   * @param {Array} headers
+   */
+  post(url, body, headers = undefined) {
+    // HTTPヘッダ情報を設定する
+    const header = headers || [
+      { name: 'Content-Type', value: ENCTYPE.URLENCODED },
+    ];
+    this.send(REQUEST_METHOD.POST, url, header, body);
   }
 
   /**
    * リクエストの送信
+   * @param {String} method
+   * @param {String} url
+   * @param {Array} headers
+   * @param {Object|String} body
    */
-  send() {
+  send(method, url, headers = undefined, body = undefined) {
+    const xhr = new XMLHttpRequest();
+
     // イベントの登録
     // (open関数の前に記載すること)
-    this.xhr.addEventListener('load', this.success);
-    this.xhr.addEventListener('error', this.error);
+    xhr.addEventListener('load', this.success);
+    xhr.addEventListener('error', this.error);
 
     // 接続を開く
-    this.xhr.open(this.method, this.url);
-    this.xhr.send();
+    xhr.open(method, url);
+
+    // リクエストヘッダを設定する
+    // (open関数の後に記載すること)
+    Object.keys(headers).forEach((key) => {
+      xhr.setRequestHeader(headers[key].name, headers[key].value);
+    });
+
+    // リクエストを送信する
+    if (method === REQUEST_METHOD.GET) {
+      xhr.send();
+    } else if (method === REQUEST_METHOD.POST) {
+      xhr.send(body);
+    } else {
+      xhr.send();
+    }
   }
 
   /**
    * 成功時の処理
+   *
+   * @returns {Ajax}
    */
   success() {
-    if (this.status === 200) {
+    if (this.status === RESPONSE_STATUS_CODE.OK) {
       console.log(this.status);
     }
-    console.log(this);
+    return this;
   }
 
   /**
    * 失敗時の処理
+   *
+   * @returns {Ajax}
    */
   error() {
-    console.log(this);
+    return this;
   }
 }
